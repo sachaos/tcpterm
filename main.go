ddafdebc0ff4a641b182d8ab3b98f89c2034e59f
Takumasa Sakao <takumasa.sakao@aktsk.jp>
2018-02-12 22:32:51 +0900

    :+1: Import urfave/cli

--- main.go
+++ main.go
@@ -1,31 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
+	"github.com/urfave/cli"
 )
 
 var (
-	device       string = "lo"
-	snapshot_len int32  = 1024
-	promiscuous  bool   = false
+	snapshot_len int32 = 1024
+	promiscuous  bool  = false
 	err          error
 	timeout      time.Duration = 100 * time.Millisecond
 	handle       *pcap.Handle
 )
 
 func main() {
-	handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
-	if err != nil {
-		log.Fatal(err)
+	var device string = "lo"
+
+	app := cli.NewApp()
+	app.Name = "tcpterm"
+	app.Usage = "tcpdump for human"
+	app.Version = "0.0.1"
+
+	app.Action = func(c *cli.Context) error {
+		handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer handle.Close()
+
+		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+
+		tcpterm := NewTcpterm(packetSource)
+		tcpterm.Run()
+		return nil
 	}
-	defer handle.Close()
 
-	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-
-	app := NewTcpterm(packetSource)
-	app.Run()
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		os.Exit(1)
+	}
 }
